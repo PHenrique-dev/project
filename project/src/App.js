eabc56391a5221165660e2d43ad40b93718a8e83
PHenrique-dev <pedrinho03s4mini@gmail.com>
2023-11-06 14:51:45 -0300

    Alteração

--- project/src/App.js
+++ project/src/App.js
@@ -1,13 +1,13 @@
 import React from "react";
 import GlobalStyle from "./styles/global";
 import RouteApp from "./rotes";
-
+import { AuthProvider } from "./contexts/auth";
 const App = () =>{
     return(
-        <>
+    <AuthProvider>
      <RouteApp/>
      <GlobalStyle/>
-     </>
+     </AuthProvider>
     )
 };
 export default App
