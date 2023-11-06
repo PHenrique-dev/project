0a6aeed64fad706a8776d9ad83978ea0fc34b2ba
PHenrique-dev <pedrinho03s4mini@gmail.com>
2023-11-06 14:42:14 -0300

    Inicio

---
+++ project/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import GlobalStyle from "./styles/global";
+import RouteApp from "./rotes";
+
+const App = () =>{
+    return(
+        <>
+     <RouteApp/>
+     <GlobalStyle/>
+     </>
+    )
+};
+export default App
